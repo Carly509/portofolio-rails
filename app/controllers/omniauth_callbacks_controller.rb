eb4e7dadba6d94e6082736b4c7a6b3cc36d53402
TAZZ <42774502+tazz509@users.noreply.github.com>
2019-04-08 14:53:15 -0400

    Create omniauth_callbacks_controller.rb

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def facebook
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+  
+      if @user.persisted?
+        current_user = @user #### I added this but i think it makes sense.
+        session[:user] = @user
+        redirect_to root_path
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
+    end
+  
+    def failure
+      redirect_to root_path
+    end
+  end
